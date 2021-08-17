ffa6cae7572555bb224092d33f047400f05d53b7
eupython <jhabhawana91@gmail.com>
2021-08-16 22:06:24 -0700

    system information

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 "this module is the main module"
 
 def application_version():
-    return "v1.1"
+return "v1.1"
+
+def get_system_information():
+    pass
 
 def main():
     print("welome to inventory")
